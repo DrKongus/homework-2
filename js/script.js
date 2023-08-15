325115ccfae775b011534568b01361a8b6075172
Fedor <f.tkachov@gmail.com>
2023-08-15 17:44:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,86 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// const exrc = "-3";
+// if (exrc >= 0) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// const exrc = true;
+// if (exrc === "test") {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// const exrc = 13;
+// if (exrc >= 10) {
+//   console.log(exrc - 5);
+// } else {
+//   console.log(exrc + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const exrc = prompt("Введіть число від 1 до 12");
+// switch (exrc) {
+//   case "1":
+//     alert(`Січень`);
+//     break;
+//   case "2":
+//     alert(`Лютий`);
+//     break;
+//   case "3":
+//     alert(`Березень`);
+//     break;
+//   case "4":
+//     alert(`Квітень`);
+//     break;
+//   case "5":
+//     alert(`Травень`);
+//     break;
+//   case "6":
+//     alert(`Червень`);
+//     break;
+//   case "7":
+//     alert(`Липень`);
+//     break;
+//   case "8":
+//     alert(`Серпень`);
+//     break;
+//   case "9":
+//     alert(`Вересень`);
+//     break;
+//   case "10":
+//     alert(`Жовтень`);
+//     break;
+//   case "11":
+//     alert(`Листопад`);
+//     break;
+//   case "12":
+//     alert(`Грудень`);
+//     break;
+
+//   default:
+//     alert(`error`);
+// }
+
+// Зробіть сервіс який отримує тризначне число
+// Поверніть користувачу сумму цих чисел
+
+// const num1 = prompt("Введіть перше число") * 1;
+// const num2 = prompt("Введіть друге число") * 1;
+// const num3 = prompt("Введіть третє число") * 1;
+// const sum = num1 + num2 + num3;
+// console.log(sum);
+// alert(sum);
